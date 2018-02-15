e8ff14d882dd3b120739df2b177c0a92e3407989
Veronica Bustabad <vbustabad@gmail.com>
2018-02-15 17:54:12 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  if block_given?
+    i = 0
+
+    while i < collection.length
+      yield(collection[i])
+      i += 1
+    end
+
+    collection
+  else
+    "This block should not run!"
+  end
 end
