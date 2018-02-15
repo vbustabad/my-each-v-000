ffb76d986e31c9601f163ddfe96da3dc6a969a70
Veronica Bustabad <vbustabad@gmail.com>
2018-02-15 17:42:58 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,13 @@
 def my_each(collection)
   i = 0
+  if block_given?
 
   while i < collection.length do |word|
     puts word
     i += 1
+  end
+
+  else
+    puts "This block should not run!"
   end
 end
