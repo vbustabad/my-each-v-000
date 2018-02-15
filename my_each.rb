6f97e8bb0578228da0de88bcff30315f4c0c59a5
Veronica Bustabad <vbustabad@gmail.com>
2018-02-15 17:50:54 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,13 @@
 def my_each(collection)
-  i = 0
   if block_given?
+    i = 0
 
-  while i < collection.length
-    yield(collection[i])
-    i += 1
-  end
+    while i < collection.length
+      yield(collection[i])
+      i += 1
+    end
 
+    array
   else
     puts "This block should not run!"
   end
